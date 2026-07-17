DouweBos/Bravo

feat(tvos): add apple tv support

maestro-client/src/main/java/maestro/device/DeviceService.kt
@@ -35,14 +35,14 @@ object DeviceService {
         connectedDevices: Set<String> = setOf()
     ): Device.Connected {
         when (device.deviceSpec.platform) {
-            Platform.IOS -> {
+            Platform.IOS, Platform.TVOS -> {
                 PrintUtils.message("Launching Simulator...")
                 try {
-                    val iosSpec = device.deviceSpec as DeviceSpec.Ios
+                    val locale = device.deviceSpec.locale
                     localSimulatorUtils.bootSimulator(device.modelId)
-                    PrintUtils.message("Setting the device locale to ${iosSpec.locale.code}...")
-                    localSimulatorUtils.setDeviceLanguage(device.modelId, iosSpec.locale.languageCode)
-                    localSimulatorUtils.setDeviceLocale(device.modelId, iosSpec.locale.code)
+                    PrintUtils.message("Setting the device locale to ${locale.code}...")
+                    localSimulatorUtils.setDeviceLanguage(device.modelId, locale.languageCode)
+                    localSimulatorUtils.setDeviceLocale(device.modelId, locale.code)
                     localSimulatorUtils.reboot(device.modelId)
                     localSimulatorUtils.launchSimulator(device.modelId)
                     localSimulatorUtils.awaitLaunch(device.modelId)
@@ -153,7 +153,7 @@ object DeviceService {
 
      fun listDevices(includeWeb: Boolean, host: String? = null, port: Int? = null): List<Device> {
         return listAndroidDevices(host, port) +
-                listIOSDevices() +
+                listAppleDevices() +
                 if (includeWeb) {
                     listWebDevices()
                 } else {
@@ -328,7 +328,7 @@ object DeviceService {
         return result
     }
 
-    fun listIOSDevices(): List<Device> {
+    fun listAppleDevices(): List<Device> {
         val simctlList = try {
             localSimulatorUtils.list()
         } catch (ignored: Exception) {
@@ -345,10 +345,10 @@ object DeviceService {
                 runtime.value
                     .filter { it.isAvailable }
                     .map { device(runtimeNameByIdentifier, runtime, it) }
-            } + listIOSConnectedDevices()
+            } + listAppleConnectedDevices()
     }
 
-    fun listIOSConnectedDevices(): List<Device.Connected> {
+    fun listAppleConnectedDevices(): List<Device.Connected> {
         val connectedIphoneList = LocalIOSDevice().listDeviceViaDeviceCtl()
 
         return connectedIphoneList.mapNotNull { device ->
@@ -385,29 +385,31 @@ object DeviceService {
         val model = device.deviceTypeIdentifier?.substringAfterLast(".") ?: ""
         // "com.apple.CoreSimulator.SimRuntime.iOS-17-5" → "iOS-17-5"
         val os = runtime.key.substringAfterLast(".")
+        // iPhone-XS -> iOS, Apple-TV-4K-3rd-generation-4K -> tvOS
+        val platform = if (model.contains("Apple-TV") == true) Platform.TVOS else Platform.IOS
 
         return if (device.state == "Booted") {
             Device.Connected(
                 instanceId = device.udid,
                 description = description,
-                platform = Platform.IOS,
+                platform = platform,
                 deviceType = Device.DeviceType.SIMULATOR,
                 deviceSpec = if (model.isBlank() || os.isBlank()) {
-                    DeviceSpec.Ios.DEFAULT
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos.DEFAULT else DeviceSpec.Ios.DEFAULT
                 } else {
-                    DeviceSpec.Ios(model = model, os = os)
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos(model = model, os = os) else DeviceSpec.Ios(model = model, os = os)
                 }
             )
         } else {
             Device.AvailableForLaunch(
                 modelId = device.udid,
                 description = description,
-                platform = Platform.IOS,
+                platform = platform,
                 deviceType =  Device.DeviceType.SIMULATOR,
                 deviceSpec = if (model.isBlank() || os.isBlank()) {
-                    DeviceSpec.Ios.DEFAULT
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos.DEFAULT else DeviceSpec.Ios.DEFAULT
                 } else {
-                    DeviceSpec.Ios(model = model, os = os)
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos(model = model, os = os) else DeviceSpec.Ios(model = model, os = os)
                 }
             )
         }
@@ -418,7 +420,7 @@ object DeviceService {
      */
     fun isDeviceConnected(deviceName: String, platform: Platform): Device.Connected? {
         return when (platform) {
-            Platform.IOS -> listIOSDevices()
+            Platform.IOS, Platform.TVOS -> listAppleDevices()
                 .filterIsInstance<Device.Connected>()
                 .find { it.description.contains(deviceName, ignoreCase = true) }
 
@@ -443,8 +445,8 @@ object DeviceService {
      * @return true if ios simulator or android emulator is available to launch
      */
     fun isDeviceAvailableToLaunch(deviceName: String, platform: Platform): Device.AvailableForLaunch? {
-        return if (platform == Platform.IOS) {
-            listIOSDevices()
+        return if (platform == Platform.IOS || platform == Platform.TVOS) {
+            listAppleDevices()
                 .filterIsInstance<Device.AvailableForLaunch>()
                 .find { it.description.contains(deviceName, ignoreCase = true) }
         } else {

plexinc/Maestro

maestro-client/src/main/java/maestro/device/DeviceService.kt
@@ -4,6 +4,7 @@ import maestro.DeviceConnectionException
 import maestro.android.AndroidDeviceConnection
 import maestro.device.util.AndroidEnvUtils
 import maestro.device.util.AvdDevice
+import maestro.device.locale.IosLocale
 import maestro.device.util.PrintUtils
 import maestro.drivers.AndroidDriver
 import maestro.drivers.CdpWebDriver
@@ -35,14 +36,14 @@ object DeviceService {
         connectedDevices: Set<String> = setOf()
     ): Device.Connected {
         when (device.deviceSpec.platform) {
-            Platform.IOS -> {
+            Platform.IOS, Platform.TVOS -> {
                 PrintUtils.message("Launching Simulator...")
                 try {
-                    val iosSpec = device.deviceSpec as DeviceSpec.Ios
+                    val locale = device.deviceSpec.locale as IosLocale
                     localSimulatorUtils.bootSimulator(device.modelId)
-                    PrintUtils.message("Setting the device locale to ${iosSpec.locale.code}...")
-                    localSimulatorUtils.setDeviceLanguage(device.modelId, iosSpec.locale.bcp47Tag)
-                    localSimulatorUtils.setDeviceLocale(device.modelId, iosSpec.locale.code)
+                    PrintUtils.message("Setting the device locale to ${locale.code}...")
+                    localSimulatorUtils.setDeviceLanguage(device.modelId, locale.bcp47Tag)
+                    localSimulatorUtils.setDeviceLocale(device.modelId, locale.code)
                     localSimulatorUtils.reboot(device.modelId)
                     localSimulatorUtils.launchSimulator(device.modelId)
                     localSimulatorUtils.awaitLaunch(device.modelId)
@@ -158,7 +159,7 @@ object DeviceService {
 
      fun listDevices(includeWeb: Boolean, host: String? = null, port: Int? = null): List<Device> {
         return listAndroidDevices(host, port) +
-                listIOSDevices() +
+                listAppleDevices() +
                 if (includeWeb) {
                     listWebDevices()
                 } else {
@@ -336,7 +337,7 @@ object DeviceService {
         return result
     }
 
-    fun listIOSDevices(): List<Device> {
+    fun listAppleDevices(): List<Device> {
         val simctlList = try {
             localSimulatorUtils.list()
         } catch (ignored: Exception) {
@@ -353,10 +354,10 @@ object DeviceService {
                 runtime.value
                     .filter { it.isAvailable }
                     .map { device(runtimeNameByIdentifier, runtime, it) }
-            } + listIOSConnectedDevices()
+            } + listAppleConnectedDevices()
     }
 
-    fun listIOSConnectedDevices(): List<Device.Connected> {
+    fun listAppleConnectedDevices(): List<Device.Connected> {
         val connectedIphoneList = LocalIOSDevice().listDeviceViaDeviceCtl()
 
         return connectedIphoneList.mapNotNull { device ->
@@ -393,29 +394,31 @@ object DeviceService {
         val model = device.deviceTypeIdentifier?.substringAfterLast(".") ?: ""
         // "com.apple.CoreSimulator.SimRuntime.iOS-17-5" → "iOS-17-5"
         val os = runtime.key.substringAfterLast(".")
+        // iPhone-XS -> iOS, Apple-TV-4K-3rd-generation-4K -> tvOS
+        val platform = if (model.contains("Apple-TV") == true) Platform.TVOS else Platform.IOS
 
         return if (device.state == "Booted") {
             Device.Connected(
                 instanceId = device.udid,
                 description = description,
-                platform = Platform.IOS,
+                platform = platform,
                 deviceType = Device.DeviceType.SIMULATOR,
                 deviceSpec = if (model.isBlank() || os.isBlank()) {
-                    DeviceSpec.Ios.DEFAULT
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos.DEFAULT else DeviceSpec.Ios.DEFAULT
                 } else {
-                    DeviceSpec.Ios(model = model, os = os)
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos(model = model, os = os) else DeviceSpec.Ios(model = model, os = os)
                 }
             )
         } else {
             Device.AvailableForLaunch(
                 modelId = device.udid,
                 description = description,
-                platform = Platform.IOS,
+                platform = platform,
                 deviceType =  Device.DeviceType.SIMULATOR,
                 deviceSpec = if (model.isBlank() || os.isBlank()) {
-                    DeviceSpec.Ios.DEFAULT
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos.DEFAULT else DeviceSpec.Ios.DEFAULT
                 } else {
-                    DeviceSpec.Ios(model = model, os = os)
+                    if (platform == Platform.TVOS) DeviceSpec.Tvos(model = model, os = os) else DeviceSpec.Ios(model = model, os = os)
                 }
             )
         }
@@ -426,7 +429,7 @@ object DeviceService {
      */
     fun isDeviceConnected(deviceName: String, platform: Platform): Device.Connected? {
         return when (platform) {
-            Platform.IOS -> listIOSDevices()
+            Platform.IOS, Platform.TVOS -> listAppleDevices()
                 .filterIsInstance<Device.Connected>()
                 .find { it.description.contains(deviceName, ignoreCase = true) }
 
@@ -455,8 +458,8 @@ object DeviceService {
      * @return true if ios simulator or android emulator is available to launch
      */
     fun isDeviceAvailableToLaunch(deviceName: String, platform: Platform): Device.AvailableForLaunch? {
-        return if (platform == Platform.IOS) {
-            listIOSDevices()
+        return if (platform == Platform.IOS || platform == Platform.TVOS) {
+            listAppleDevices()
                 .filterIsInstance<Device.AvailableForLaunch>()
                 .find { it.description.contains(deviceName, ignoreCase = true) }
         } else {